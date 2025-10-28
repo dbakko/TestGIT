4960ecc5226e768147c350a2a70e8f340b44b553
Daniel <danielpatryk.bak@studio.unibo.it>
2025-10-28 15:37:39 +0100

    Ripristinato

--- UseCIau.java
+++ UseCIau.java
@@ -1,5 +1,5 @@
 public class UseCIau {
     public static void main(String[] args) {
-        System.out.println("Hello, Maiale da branch!, porco!!");
+        System.out.println("Hello, World!");
     }
 }
